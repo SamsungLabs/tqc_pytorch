ada93bd982abfc9d70849b1ed5566623f9f55d99
AlexanderGri <grishin.alexgri@yandex.ru>
2019-11-28 17:55:33 +0300

    Fix quantiles computation

--- multiq/functions.py
+++ multiq/functions.py
@@ -27,6 +27,6 @@ def quantile_huber_loss_f(quantiles, samples):
                              pairwise_delta ** 2 * 0.5)
 
     n_quantiles = quantiles.shape[2]
-    tau = torch.arange(n_quantiles, device=DEVICE).float() + 1 / 2 / n_quantiles
+    tau = torch.arange(n_quantiles, device=DEVICE).float() / n_quantiles + 1 / 2 / n_quantiles
     loss = (torch.abs(tau[None, None, :, None] - (pairwise_delta < 0).float()) * huber_loss).mean()
     return loss
